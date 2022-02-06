fc187a17237e4dc0c5e8ae3b81a79d6c44186000
JeFY6gWc <rus-baishev@yandex.ru>
2022-02-06 14:36:06 +0400

    commit to new_f2

---
+++ Lab1_2.py
@@ -0,0 +1,30 @@
+#Подключение библиотеки с готовыми функциями сортировки
+import sorting
+import random
+fl = False
+while not fl:
+		print('1 - Ввод с клавиатуры, 2 - генерация случайных чисел')
+		#Ввод выбранного оператора:
+		choose_input = int(input())
+		#Проверка на выбранный вариант:
+		if choose_input == 1:
+			change_list = []
+			#Формирование списка через клавиатуру
+			change_list = [int(i) for i in input('Введите элементы списка через пробел: ').split()]
+			print(change_list)
+			#Вывод отсортированного массива
+			res = sorting.selection(change_list)
+			print('Сортировка выбором   : ', res)
+		if choose_input == 2:
+			change_list = []
+			print('Введите размер списка:')
+			#Ввод размера случайного списка
+			ran_num = int(input())
+			for i in range(0,int(ran_num)):
+				n = random.randint(1,30)
+				change_list.append(n)
+			print(change_list)
+			#Вывод отсортированного массива
+			res = sorting.selection(change_list)
+			print('Сортировка выбором   : ', res)
+			
